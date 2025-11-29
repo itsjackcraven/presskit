420bd51d14eb52d2b947608c8d100527f8f71ef7
itsjackcraven <jackcraven5225@gmail.com>
2025-11-29 09:42:46 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+
+function openLightbox(src) {
+  document.getElementById('lightbox').style.display = 'flex';
+  document.getElementById('lightbox-img').src = src;
+}
+
+function closeLightbox() {
+  document.getElementById('lightbox').style.display = 'none';
+}
